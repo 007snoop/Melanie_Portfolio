31d40b42ab641b10d0797236897a54e7a3e5775c
Colin Yetman <colinguitarist@gmail.com>
2026-02-25 12:56:23 -0330

    add session restriction to edit page

--- public/edit_profile.php
+++ public/edit_profile.php
@@ -1,4 +1,9 @@
 <?php 
+ini_set('session.cookie_httponly', 1);
+ini_set('session.use_strict_mode', 1);
+
+session_start();
+
 require_once __DIR__ . "/../src/profileRepo.php";
 
 $repo = new profileRepo();
